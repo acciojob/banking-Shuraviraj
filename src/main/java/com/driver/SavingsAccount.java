1616a7723f4327b639b573881a42fa1389248486
Ravi Raj Singh <ravi.raj@windmill.digital>
2023-08-25 21:09:50 +0530

    Attempt 10

--- src/main/java/com/driver/SavingsAccount.java
+++ src/main/java/com/driver/SavingsAccount.java
@@ -7,6 +7,7 @@ public class SavingsAccount extends BankAccount {
     public SavingsAccount(String name, double balance, double maxWithdrawalLimit, double rate) {
         // minimum balance is 0 by default
         super(name, balance, 0);
+        this.rate = rate;
         this.maxWithdrawalLimit = maxWithdrawalLimit;
     }
 
